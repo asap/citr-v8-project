1b54ae3683b22ab1e7e330098dceea9dd1f2d050
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2024-12-27 13:47:54 -0500

    Add Types to Details and API Responses

---
+++ 14-context/src/AdoptedPetContext.ts
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { Pet } from './APIResponsesTypes'
+
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+    {
+        id: 1337,
+        name: 'Fido',
+        animal: "dog",
+        description: "Lorem Ipsum",
+        breed: "beagle",
+        images: [],
+        city: "Jersey City",
+        state: "NJ"
+    },
+    () => { },
+]);
+
+export default AdoptedPetContext;
