6d7b6f6b3c5d842bc8848a5dd5bd269ee5a177c8
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2024-12-27 14:44:04 -0500

    Add remaining types

--- 14-context/src/APIResponsesTypes.ts
+++ 14-context/src/APIResponsesTypes.ts
@@ -1,20 +1,25 @@
-export type Animal = "dog"  | "cat" | "bird" | "reptile" | "rabbit"
+export type Animal = "dog" | "cat" | "bird" | "reptile" | "rabbit";
 
-export interface Pet { 
-    id: number;
-    name: string;
-    description: string;
-    animal: Animal;
-    breed: string;
-    images: string[];
-    city: string;
-    state: string
+export interface Pet {
+  id: number;
+  name: string;
+  description: string;
+  animal: Animal;
+  breed: string;
+  images: string[];
+  city: string;
+  state: string;
 }
 
 export interface PetAPIResponse {
-    numberOfResults: number;
-    startIndex: number;
-    endIndex: number;
-    hasNext: boolean;
-    pets: Pet[];
+  numberOfResults: number;
+  startIndex: number;
+  endIndex: number;
+  hasNext: boolean;
+  pets: Pet[];
+}
+
+export interface BreedListAPIResponse {
+  animal: Animal;
+  breeds: string[];
 }
